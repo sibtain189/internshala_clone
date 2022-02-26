8cf28ed089e7d5d3273aee7bc75ffd65cc06fe65
Suraj <rauniyar.suraj731@gmail.com>
2022-02-26 13:08:05 +0530

    Done

--- internship_page/components/sidebar.js
+++ internship_page/components/sidebar.js
@@ -27,24 +27,39 @@ function sidebar(){
         </select>
           <br>
           <label for="">Location</label>
-          
+          <br>
           <select type="text" placeholder="e.g.Delhi " style="line-height: -15px;color:gray;border:1;appearance: none;"  />
-          <option value="e.g. Marketing">e.g.Delhi </option>
-          <option value="">.Mumbai</option>
+          <option value="e.g. place">e.g.Delhi </option>
+          <option value="">Agra</option>
+          <option>Adilabad</option>
+          <option>Agar</option>
+          <option>Agartala</option>
+          <option>Ahmedabad</option>
+          <option>Ahmadnagar</option>
+          <option>Ajmer</option>
+          <option>Akola</option>
+          <option>Allahabad</option>
+          <option>Badarpur</option>
+          <option>Bahadurpur</option>
+          <option>Balghat</option>
+          <option>Balasore</option>
+          <option>Balangir</option>
+          <option>Banaras</option>
+          <option>Banke</option>
+          <option>Baroda</option>
+          <option>Bhatinda</option>
+          <option>Balamau</option>
           <option>Bangalore</option>
           <option>Delhi</option>
           <option>Hyderabad</option>
           <option>Pune</option>
           <option>Chennai</option>
           <option>Calcutta</option>
-          <option>Android App Development</option>
-          <option>Angular.js Development</option>
-          <option>Animation</option>
-          <option>Architecture</option>
-          <option>Artificial Intelligence(AI)</option>
-          <option>ASP.NET Development</option>
-          <option>Audio Making/Editing</option>
+          <option>Rajasthan</option>
+          <option>Mumbai</option>
+          
         </select>
+        <br>
         </form>
         <div id="toggle-switch">
           <label id = "text_label">Work from Home</label>
@@ -66,7 +81,7 @@ function sidebar(){
           </div>
         </div>
         <div>
-          <label>Desired minimum monthly stipend(₹)</label>
+          <label>Desired minimum monthly stipend ( ₹ )</label>
           <input
             type="range"
             min="0"
@@ -89,10 +104,14 @@ function sidebar(){
         </div>
         <form action="">
           <label for="">Starting from (or after)</label>
-          <input type="text" placeholder="Choose date" />
+          <select type="text" placeholder="e.g.Delhi " style="line-height: -15px;color:gray;border:1;appearance: none;"  />
+          <option>Date</option>
+          </select>
           <br>
           <label for=""> Max. duration (months)</label>
-          <input type="months" placeholder="Choose duration" />
+          <select type="months" placeholder="Choose duration" style="line-height: -15px;color:gray;border:1;appearance: none;" >
+          <option>Duration</option>
+          </select>
         </form>
         <div>
            <div id="toggle-switch">
@@ -116,6 +135,8 @@ function sidebar(){
           </div>
          </div>
         </div>
+        <p>-------------------------  OR  -------------------------</p>
+        <input type="text" placeholder="e.g. Design,Mumbai,Infosys"/>
     `
 }
 
